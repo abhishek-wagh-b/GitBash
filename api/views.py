560fd45143a155893e4fa54b867872b8dfc4bc81
Tushpatil18 <tusharpatil181111@gmail.com>
2025-08-01 16:53:51 +0530

    ok now in this new commit i have set user authentication basically only authrized user will able to access blogpost api

--- api/views.py
+++ api/views.py
@@ -3,16 +3,28 @@ from rest_framework import generics, status
 from rest_framework.response import Response
 from .models import Blogpost
 from .serializers import BlogpostSerializers
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.views import APIView
+from .permissions import IsAdminUser
+
+class AdminOnlyView(APIView):
+    permission_classes = [IsAdminUser] 
+
+    def get(self, request):
+        return Response({"message": "Hello, Admin!"})
 
 class BlogpostView(generics.ListCreateAPIView):
     queryset=Blogpost.objects.all()
     serializer_class=BlogpostSerializers
+    permission_classes = [IsAuthenticated]
 
     def delete(self, request, *args, **kwrgs):
-          Blogpost.objects.all().delete()
-          return Response(status=status.HTTP_204_NO_CONTENT)
+        if not request.user.is_staff:
+            return Response({"detail": "Only admins can delete."}, status=status.HTTP_403_FORBIDDEN)
+        
 
 class Blogpostupdatedestry(generics.RetrieveUpdateDestroyAPIView):
         queryset=Blogpost.objects.all()
         serializer_class=BlogpostSerializers
         lookup_field="pk"
+        permission_classes = [IsAuthenticated]
